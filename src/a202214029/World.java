99377e6f30c4315c54ad1bc4135cc4f7e7a069e1
Joe Sanghyeok <matin4998@gmail.com>
2026-04-01 11:39:00 +0900

    five

--- src/a202214029/World.java
+++ src/a202214029/World.java
@@ -2,5 +2,7 @@ public class World {
     public static void run() { 
         System.out.println("202214029"); 
     }
-
+    public static void run2() {
+        System.out.println("hello world");
+    }
 } 
